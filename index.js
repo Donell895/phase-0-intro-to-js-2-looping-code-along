6a24052fb9a21cbafcc242860a114c5ad35e2236
donell <donell.wambua@student.moringaschool.com>
2023-06-18 22:23:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessages = ["Thank you, Guadalupe, for the wonderful surprise gift!"
+]
+    
+    for (let i = 0; i < names.length; i++) {
+      const name=(Ollie)
+      const message = `Thank you, ${names[Ollie]}, for the wonderful gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+  }
+  
+  // Function to count down from a given number
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
